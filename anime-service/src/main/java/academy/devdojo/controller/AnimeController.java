444cbd3233fbf0f6c2a251e2390552187146a3e5
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-04-18 18:16:29 -0300

    video 10: Threads Virtuais, Stacktrace e Tomcat Threads

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,18 +1,22 @@
 package academy.devdojo.controller;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("v1/animes")
 public class AnimeController {
 
     @GetMapping
     public List<String> listAll(){
+        log.info(Thread.currentThread().getName());
         return List.of("Ninja kamui", "Kaijuu-8gou");
     }
 }
+
--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -3,7 +3,6 @@ package academy.devdojo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class HelloController {
