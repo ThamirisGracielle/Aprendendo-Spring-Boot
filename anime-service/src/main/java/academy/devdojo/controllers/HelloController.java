544b56662401f0faaea323b215572b36df983ac9
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-04-18 13:32:13 -0300

    video07: RequestMapping e Servlet Context

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @GetMapping
     public String hi(){
-
         return "OMAE WA MOU SINDE IRU";
-
     }
-
-
 
 }
