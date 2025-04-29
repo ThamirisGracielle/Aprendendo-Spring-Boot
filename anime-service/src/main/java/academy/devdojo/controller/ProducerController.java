614ed08ef9ce4541badfff7e3fcf5ec0c1521851
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-04-29 05:41:38 -0300

    video21: Atualizando Producer

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -1,8 +1,10 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
+import academy.devdojo.domain.Producer;
 import academy.devdojo.mapper.ProducerMapper;
 import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.response.ProducerGetResponse;
 import academy.devdojo.response.ProducerGetResponse;
 import org.slf4j.Logger;
 import org.springframework.http.HttpHeaders;
@@ -22,19 +24,29 @@ public class ProducerController {
     private static final ProducerMapper MAPPER = ProducerMapper.INSTANCE;
 
     @GetMapping
-    public List<Producer> listAll(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<ProducerGetResponse>> listAll(@RequestParam(required = false) String name) {
+        log.debug("Request received to list all producers, param name '{}", name);
         var producers = Producer.getProducers();
-        if (name == null) return producers;
+        var producerGetResponseList = MAPPER.toProducerGetRresponseList(producers);
+        if (name == null) return ResponseEntity.ok(producerGetResponseList);
 
-        return producers.stream().
-                filter(producer -> producer.getName()
-                        .equalsIgnoreCase(name)).toList();
+        var response = producerGetResponseList
+                .stream()
+                .filter(producer -> producer.getName().equalsIgnoreCase(name))
+                .toList();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("{id}")
-    public Producer findById(@PathVariable Long id) {
-        return Producer.getProducers().stream().filter(producer -> producer.getId().equals(id))
-                .findFirst().orElse(null);
+    public ResponseEntity<Producer> findById(@PathVariable Long id) {
+        log.debug("Requeste to find by id: {}", id);
+
+        var producerGetResponse =  Producer.getProducers()
+                .stream().filter(producer -> producer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return ResponseEntity.ok(producerGetResponse);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
--- anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
+++ anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
@@ -8,6 +8,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Mapper(imports = {LocalDateTime.class, ThreadLocalRandom.class})
@@ -20,4 +21,6 @@ public interface ProducerMapper {
     Producer toProducer(ProducerPostRequest postRequest);
 
     ProducerGetResponse toProducerGetResponse(Producer producer);
+
+    List<ProducerGetResponse> toProducerGetRresponseList(List<Producer> producers);
 }
