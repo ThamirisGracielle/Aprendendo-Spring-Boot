7ae546e7cda612a85e76ffc21a67a45351220cca
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-02 15:00:26 -0300

    video 31: EXercicio 08 Injeção de Dependência

---
+++ anime-service/exercises/Exercise08.md
@@ -0,0 +1,4 @@
+# Exercicise 8 MVC Dependency Injection
+
+
+Refatore o dmínio Anime para que todas as classes utilizem Injeção de Dependência
--- anime-service/src/main/java/academy/devdojo/mapper/AnimeMapper.java
+++ anime-service/src/main/java/academy/devdojo/mapper/AnimeMapper.java
@@ -5,15 +5,16 @@ import academy.devdojo.request.AnimePostRequest;
 import academy.devdojo.request.AnimePutRequest;
 import academy.devdojo.response.AnimeGetResponse;
 import academy.devdojo.response.AnimePostResponse;
+import ch.qos.logback.core.model.ComponentModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper
+@Mapper (componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AnimeMapper {
-    AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
 
     @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
     Anime toAnime(AnimePostRequest postRequest);
